6863577c3a59544431405c6df7b315da3a078d7b
admin <miner.vip@gmail.com>
2025-02-21 00:35:08 +0800

    v0.6.3 - 下载优化

--- install.sh
+++ install.sh
@@ -12,6 +12,8 @@ message=""
 farmid=""
 use_ip_as_hostname="false"
 down_uri="https://minerx-download.oss-cn-shanghai.aliyuncs.com"
+backup_down_uri="http://47.97.210.214:8889"
+
 
 ## 遍历参数
 while [[ $# -gt 0 ]]; do
@@ -31,97 +33,54 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
+##################################################################
+## 下载文件并提取
+##################################################################
+download_file() {
+    local uri=$1
+    local version_file="${uri}/VERSION"
+    local filename
+    local url
+    local archive
 
-# ##################################################################
-# ## Sync Time
-# ##################################################################
-# 
-# # 时间服务器列表
-# servers=("http://www.apple.com" "http://www.baidu.com" "http://www.google.com")
-# 
-# # 允许的时间差（秒）
-# time_tolerance=5
-# 
-# # 获取 HTTP 时间
-# get_http_time() {
-#     local server=$1
-#     local http_time
-# 
-#     # 获取 HTTP 响应的日期头信息并转换为时间戳
-#     http_time=$(curl -sI "$server" | grep -i "^date:" | awk '{for (i=2; i<=NF; i++) printf $i" "; print ""}' | xargs -I{} date -d "{}" +%s 2>/dev/null)
-# 
-#     if [[ -z "$http_time" ]]; then
-#         echo "Failed to fetch time from $server"
-#         return 1
-#     fi
-# 
-#     echo "$http_time"
-#     return 0
-# }
-# 
-# # 同步时间函数
-# sync_time() {
-#     local http_time=$1
-#     if date -s "@$http_time" >/dev/null 2>&1; then
-#         echo "Successfully synced time to $(date '+%Y-%m-%d %H:%M:%S')"
-#     else
-#         echo "Failed to sync time"
-#     fi
-# }
-# 
-# # 主函数
-# main() {
-#     # 获取本地时间戳
-#     local_time=$(date +%s)
-# 
-#     for server in "${servers[@]}"; do
-#         echo "Trying to fetch time from $server"
-#         http_time=$(get_http_time "$server")
-# 
-#         if [[ $? -eq 0 && -n "$http_time" ]]; then
-#             time_diff=$((local_time - http_time))
-#             time_diff=${time_diff#-} # 取绝对值
-# 
-#             # echo "Local time: $(date -d "@$local_time" '+%Y-%m-%d %H:%M:%S')"
-#             # echo "Server time: $(date -d "@$http_time" '+%Y-%m-%d %H:%M:%S')"
-#             # echo "Time difference: ${time_diff}s"
-# 
-#             # 如果时间差超过允许范围，则同步时间
-#             if ((time_diff > time_tolerance)); then
-#                 echo "Time difference exceeds ${time_tolerance}s. Synchronizing..."
-# 
-#                 if date -s "@$http_time" >/dev/null 2>&1; then
-#                         echo "Successfully synced time to $(date '+%Y-%m-%d %H:%M:%S')"
-#                         break
-#                 fi
-#             else
-#                 echo "Time is within acceptable range. No synchronization needed."
-#                 break
-#             fi
-#         fi
-#     done
-# 
-#     echo "All servers failed to provide valid time. No synchronization performed."
-# }
-# main
+    # 获取版本号
+    local ver=$(curl -s "$version_file" | awk -F= '{print $2}')
+    [[ -z "$ver" ]] && return 1
+    filename="os-${ver}.tar.gz"
+    url="${uri}/${filename}"
+    archive="/tmp/${filename}"  # 在这里给 archive 赋值
+
+    echoCyan "最新版本: ${ver}"
+
+    # 删除已有的下载文件
+    sudo rm -f "${archive}"
+
+    # 下载并保存
+    if sudo wget -t 5 -T 20 -c "${url}" -P /tmp/; then
+        echoCyan "下载成功: ${filename}"
+        sudo tar xzf ${archive} -C / || { echoYellow "Install failed!"; exit 1; }
+        return 0
+    else
+        echoYellow "下载失败: ${filename}"
+        return 1
+    fi
+}
+
+# 检查主下载 URI
+if ! download_file "${down_uri}"; then
+    # 主下载失败，尝试备用 URI
+    echoYellow "主下载 URI 失败，尝试备用 URI..."
+
+    if ! download_file "${backup_down_uri}"; then
+        echoRed "备用下载 URI 也失败了！"
+        exit 1
+    fi
+fi
+
 
 ##################################################################
-## Install
-##################################################################
-VER=$(curl -s ${down_uri}/VERSION | awk -F= '{print $2}')
-FILENAME="os-${VER}.tar.gz"
-URL="${down_uri}/${FILENAME}"
-
-## Download && Extract
-echoCyan "Latest version: ${VER}"
-
-ARCHIVE="/tmp/${FILENAME}"
-sudo rm -f ${ARCHIVE}
-sudo wget -t 5 -T 20 -c "${URL}" -P /tmp/ || { echoYellow "Download failed!"; exit 1; }
-sudo tar xzf ${ARCHIVE} -C / || { echoYellow "Install failed!"; exit 1; }
-
-
 ## If farmid is specified, perform an overwrite installation.
+##################################################################
 RIG_CONFIG="/os/config/rig.conf"
 if [[ -z ${farmid} ]]; then
     if [[ ! -f ${RIG_CONFIG} ]]; then
@@ -135,7 +94,9 @@ else
 fi
 
 
+##################################################################
 ## Handle Hostname
+##################################################################
 if [[ ${use_ip_as_hostname} == 'true' ]]; then
     IP=$(ip route get 8.8.8.8 | grep -oP '(?<=src\s)\d+(\.\d+){3}')
     IFS='.' read -r -a ip_parts <<< "$IP"
@@ -150,17 +111,21 @@ if [[ ${use_ip_as_hostname} == 'true' ]]; then
 fi
 
 
+##################################################################
 ## Add environment variables
+##################################################################
 NEW_PATH="/os/bin/"
 BASHRC_FILE="/etc/bash.bashrc"
 sudo sed -i "\|export PATH=.*${NEW_PATH}|d" ${BASHRC_FILE}
 echo "export PATH=${NEW_PATH}:\$PATH" | sudo tee -a ${BASHRC_FILE} > /dev/null
 
 
+##################################################################
 ## Install as a systemd service
+##################################################################
 sudo cp /os/service/os-core.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable os-core.service
 sudo systemctl restart os-core.service
 
-echoCyan "------------------------------------------------------------------ ${VER} Installation successful. ${message}"
+echoCyan "------------------------------------------------------------------ Installation successful. ${message}"
