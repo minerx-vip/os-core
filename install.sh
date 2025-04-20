5f3a2f12b5a63b43ee2382cf1833065a3826b6cc
admin <miner.vip@gmail.com>
2025-04-20 21:09:42 +0800

    v0.6.72 - supervisord 自动启动添加到 /etc/profile

--- install.sh
+++ install.sh
@@ -85,7 +85,8 @@ if [ -f /.dockerenv ] || grep -qE "docker|kubepods" /proc/1/cgroup || [[ ${isWSL
     in_container="true"
     apt update
     apt install -y iproute2 dmidecode lsb-release pciutils screen jq supervisor procps gettext libjansson-dev bc 
-    apt install -y netcat
+    apt install -y netcat  2>/dev/null
+    apt install -y netcat-openbsd  2>/dev/null
 fi
 
 ##################################################################
